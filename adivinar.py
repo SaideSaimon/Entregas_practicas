37b2268e535912b12404f689daa888431494f7cd
Simón Palacios <palaciossimon6@gmail.com>
2022-03-17 22:48:27 -0300

    Carga adivinar.py

---
+++ adivinar.py
@@ -0,0 +1,20 @@
+
+# Adivina adivinador....
+import random
+
+numero_aleatorio = random.randrange(5)
+gane = False
+print("Tenés 3 intentos para adivinar un entre 0 y 99")
+intento = 1
+max_intentos = 5
+# Iteración de intentos
+while intento <= max_intentos and not gane:
+	numero_ingresado = int(input('Ingresa tu número: '))
+	if numero_ingresado == numero_aleatorio:
+		print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
+		gane = True
+	else:
+		print('Mmmm ... No.. ese número no es... Seguí intentando.')
+	intento += 1
+if not gane:
+	print('\n Perdiste :(\n El número era: {}'.format(numero_aleatorio))

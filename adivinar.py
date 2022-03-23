2ce7c8373c4f7bc1e6fd2cf9e7e0b4cde92bec4b
Simón Palacios <palaciossimon6@gmail.com>
2022-03-22 22:53:14 -0300

    Aplicadas las normas pep8

--- adivinar.py
+++ adivinar.py
@@ -7,14 +7,13 @@ gane = False
 print("Tenés 3 intentos para adivinar un entre 0 y 99")
 intento = 1
 max_intentos = 5
-# Iteración de intentos
 while intento <= max_intentos and not gane:
-	numero_ingresado = int(input('Ingresa tu número: '))
-	if numero_ingresado == numero_aleatorio:
-		print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
-		gane = True
-	else:
-		print('Mmmm ... No.. ese número no es... Seguí intentando.')
-	intento += 1
+    numero_ingresado = int(input('Ingresa tu número: '))
+    if numero_ingresado == numero_aleatorio:
+        print(f"Ganaste! y necesitaste {intento} intentos!!!")
+        gane = True
+    else:
+        print('Mmmm ... No.. ese número no es... Seguí intentando.')
+    intento += 1
 if not gane:
-	print('\n Perdiste :(\n El número era: {}'.format(numero_aleatorio))
+    print(f"\n Perdiste :(\n El número era: {numero_aleatorio}")
